6a35efdcfbf48107dad44d304cd352abeeb1cd54
Edson de Carvalho <edsondecarvalho190@gmail.com>
2021-06-23 05:22:48 +0100

    Construindo o parte administrativa(Dashboard)

---
+++ src/components/Navbaradmin.vue
@@ -0,0 +1,56 @@
+<template>
+    <nav>
+        <v-app-bar color="red" dark app>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+
+            </v-app-bar-nav-icon>
+        <v-toolbar-title class="text-uppercase">
+            <span class="font-weight-light">MED-</span>
+            <span>ANGOLA</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text >
+            <span>Sair</span>
+            <v-icon>logout</v-icon>
+        </v-btn>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" dark app class="red darken-4">
+            <v-layout column align-center>
+                <v-flex class="mt-5">
+                    <v-avatar size="100">
+                        <v-img src="/image1.jpg"></v-img>
+                    </v-avatar>
+                    <p class="white--text subheading mt-1 text-center">Username</p>
+                </v-flex>
+            </v-layout>
+            <v-list flat>
+                <v-list-item v-for="link in links" :key="link.text" router :to="link.route" active-class="border">
+                    <v-list-item-action>
+                        <v-icon>{{link.icon}}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>{{link.text}}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
+</template>
+<script>
+export default{
+    data: () => ({
+        drawer: true,
+        links:[
+            
+            {icon: 'view_list',   text:'Ver Candidaturas', route:'#'},
+            {icon: 'leaderboard', text:'Ver Candidaturas', route:'#'},
+            {icon: 'assessment', text:'Ver Candidaturas', route:'#'}
+        ]
+    })
+}
+</script>
+<stile>
+.border{
+    border-left: 4px solid #0ba518
+}
+</stile>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,3 +1,20 @@
 <template>
+    <div>
+        <Navbaradmin/>
     <h1>Area Administrativa</h1>
+    </div>
+    
 </template>
+
+
+<script>
+  import Navbaradmin from '../components/Navbaradmin'
+
+  export default {
+    name: 'Publico',
+
+    components: {
+      Navbaradmin,
+    },
+  }
+</script>
