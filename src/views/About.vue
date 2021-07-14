0f42d2f975621a2db58822d8e8f881049da8d25b
Edson de Carvalho <edsondecarvalho190@gmail.com>
2021-07-14 17:53:22 +0100

    adicionando o about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,32 +1,30 @@
 <template>
- <div>
-      <Navbarpublico/>
-      <Footer/>
-      <h1> O Ministério da Educação como local de pesquisa, é o órgão público do governo
-responsável pela elaboração e execução da política nacional da educação (PNE). Esta
-instituição tem por objetivo preparar o indivíduo para as exigências da vida política,
-económica e social do país, e é constituída por profissionais, instituições escolares
-públicas e privadas. </h1>
-      <Map/>
-      
-      
+  <div>
+    <Navbarpublico />
+    <Footer />
+    <h1>
+      O Ministério da Educação como local de pesquisa, é o órgão público do<br>
+      governo responsável pela elaboração e execução da política nacional da<br>
+      educação (PNE). Esta instituição tem por objetivo preparar o indivíduo<br>
+      para as exigências da vida política, económica e social do país, e é<br>
+      constituída por profissionais, instituições escolares públicas e privadas.<br>
+    </h1>
+    <Map />
   </div>
 </template>
 
 <script>
-  import Navbarpublico from '../components/Navbarpublico';
-  import Footer from '../components/Footer';
-  import Map from '../components/Map';
+import Navbarpublico from "../components/Navbarpublico";
+import Footer from "../components/Footer";
+import Map from "../components/Map";
 
+export default {
+  name: "Publico",
 
-
-  export default {
-    name: 'Publico',
-
-    components: {
-      Navbarpublico,
-      Footer,
-      Map
-    },
-  }
+  components: {
+    Navbarpublico,
+    Footer,
+    Map,
+  },
+};
 </script>
